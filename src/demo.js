f2e2af5b51074bf06283697eb106b995122fb31b
majj <majj@heptax.com>
2023-01-10 17:27:14 +0800

    initial

---
+++ src/demo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class demo extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+    render() {
+        return (
+            <div>demo</div>
+        )
+    }
+}
