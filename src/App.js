acde88d62bfdcf6cb9f011e55f855a28a4954d28
majj <majj@heptax.com>
2023-01-12 09:36:16 +0800

    又修改了一下子呢

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       只是想测试一下啊啊啊啊啊啊啊啊啊啊啊啊啊～～～～～～～
       <div style={{color: 'red'}}>哈哈哈哈哈哈哈哈哈哈哈哈哈～～～～～～～</div>
+      <div style={{color: 'green'}}>哈哈哈哈哈哈哈哈哈哈哈哈哈～～～～～～～</div>
     </div>
   );
 }
